96bbba4a5bb6f00de7ce475318b149f564aede70
anand-nanwana14 <anandnanwana84@gmail.com>
2024-01-29 15:22:28 +0530

    changes

--- SecondLargestNumber.js
+++ SecondLargestNumber.js
@@ -1,5 +1,20 @@
 // Problem 1: Complete the secondLargest function which takes in an array of numbers in input and return the second biggest number in the array. (without using sort)?
 function secondLargest(array) {
-    // Write your code here
+  if (array.length < 2) {
+    return "Array should have at least two elements";
+}
+let Largest = Math.max(array[0], array[1]);
+let secondLargest = Math.min(array[0], array[1]);
+
+for (let i = 2; i < array.length; i++) {
+    if (array[i] > Largest) {
+        secondLargest = Largest;
+        Largest = array[i];
+    } else if (array[i] > secondLargest && array[i] !== Largest) {
+        secondLargest = array[i];
+    }
+}
+
+return secondLargest;
   }
   
